230bc187c86e81876045d6fa8d6094bdfd4ffffc
StanV000 <stanimir.vasilev@mycit.ie>
2026-06-22 00:40:38 +0100

    a

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include "test/vector_transform_test.cpp"
-#include "test/forward_kinematics_test.cpp"
-#include "test/ik_solver_test.cpp"
-#include "test/joint_limits_test.cpp"
+
+void runVectorTransformTest();
+void runForwardKinematicsTest();
+void runIKSolverTest();
+void runJointLimitTest();
 
 int main()
 {
